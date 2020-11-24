3c606ee7ec27bb05c401ec8a5ca8dd1a35a7631c
astapenko <astapenko>
2020-11-24 12:57:47 +0300

    refactor add button to fab, adding scrolling

--- app/src/main/java/com/example/compose/todo/TodoScreen.kt
+++ app/src/main/java/com/example/compose/todo/TodoScreen.kt
@@ -1,14 +1,19 @@
 package com.example.compose.todo
 
+import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import com.example.compose.R
 import com.example.compose.TodoListItem
+import com.example.compose.ui.red
 
 @Composable
 fun TodoScreen(
@@ -16,23 +21,22 @@ fun TodoScreen(
     onAddItem: (TodoItem) -> Unit,
     onRemoveItem: (TodoItem) -> Unit
 ) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Column(
-            modifier = Modifier.weight(1f).padding(8.dp),
+    Box(modifier = Modifier.fillMaxSize()) {
+        ScrollableColumn(
+            modifier = Modifier.padding(8.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items.forEach {
                 TodoListItem(it, Modifier.fillMaxWidth()) { }
             }
         }
-        Button(
-            modifier = Modifier.fillMaxWidth().then(Modifier.padding(16.dp)),
-            onClick = { onAddItem(TodoItem.createRandomItem()) }
-        ) {
-            Text(text = "Add random item")
-        }
+        FloatingActionButton(
+            modifier = Modifier.align(Alignment.BottomCenter).padding(PaddingValues(bottom = 16.dp)),
+            onClick = { onAddItem(TodoItem.createRandomItem()) },
+            backgroundColor = red,
+            contentColor = Color.White,
+            icon = { Icon(asset = vectorResource(id = R.drawable.ic_add)) }
+        )
     }
 }
 
